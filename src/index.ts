845e7ee75462d9cc3c3d5139e9a41b7d3b12828f
Zack Heil <57419337+zackheil@users.noreply.github.com>
2023-03-11 15:22:34 -0600

    Adding input variables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { getActionInput } from './util.js';
 
 const main = async () => {
   try {
     core.debug('Starting Action');
 
-    // Throw if event is not of type workflow_dispatch or pull_request?
+    // Get the input variables from the action
+    const context = github.context;
+    const defaultBump = getActionInput('default-bump', 'patch');
+    const prereleasePrefix = getActionInput('prerelease-prefix', 'rc.');
+    const labelPrefix = getActionInput('label-prefix', '');
+    const githubToken = core.getInput('github-token');
+    const octokit = github.getOctokit(githubToken);
+
+    console.log('context', JSON.stringify(context, null, 2));
 
     // Get the most recent tag associated with the commit on the main branch
 
@@ -13,8 +23,6 @@ const main = async () => {
     // Resolve the version number based on the labels and the most recent tag
 
     // Return the resolved version number as an output variable
-
-    console.log('Hello World');
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
---
+++ src/util.ts
@@ -0,0 +1,69 @@
+import * as core from '@actions/core';
+
+export const getActionInput = (name: string, def: string) => {
+  const input = core.getInput(name, { trimWhitespace: true });
+  if (input) {
+    core.debug(`Using user-passed input for variable ${name}. (${input})`);
+    return input;
+  }
+  core.debug(`Using default value for variable ${name}. (${input})`);
+  return def;
+};
+
+/**
+ * Will parse a semver in following formats:
+ * v1.2.3
+ * V1.2.3
+ * 1.2.3
+ * 1.2.3-suffix
+ */
+export const parseSemver = (semver: string) => {
+  if (!semver.match(/^(v|V)?\d+\.\d+\.\d+(-\w+)?$/)) {
+    throw new Error(`Invalid semver: ${semver}`);
+  }
+
+  let sv = semver.toLowerCase();
+  if (sv.startsWith('v')) {
+    sv = sv.slice(1);
+  }
+
+  const [major, minor, tail] = sv.split('.');
+  const [patch, suffix] = tail.split('-');
+
+  return {
+    major: parseInt(major),
+    minor: parseInt(minor),
+    patch: parseInt(patch),
+    suffix,
+  };
+};
+
+/**
+ * Will create a semver based upon current state, bump type, and optional prerelease suffix
+ */
+export const createSemver = (current: string, bump: 'major' | 'minor' | 'patch', prerelease?: string) => {
+  let { major, minor, patch } = parseSemver(current);
+
+  switch (bump) {
+    case 'major':
+      major += 1;
+      minor = 0;
+      patch = 0;
+      break;
+    case 'minor':
+      minor += 1;
+      patch = 0;
+      break;
+    case 'patch':
+      patch += 1;
+      break;
+  }
+
+  return {
+    string: `${major}.${minor}.${patch}${prerelease ? `-${prerelease}` : ''}`,
+    major,
+    minor,
+    patch,
+    suffix: prerelease,
+  };
+};
