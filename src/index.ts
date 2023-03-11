14243a034eacd2abcd192fd38b745eaebd0e82ae
Zack Heil <57419337+zackheil@users.noreply.github.com>
2023-03-10 23:27:36 -0600

    Scaffolding out steps for action

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
+import * as core from '@actions/core';
 import { sayHello } from './hello.js';
 
 const main = async () => {
-  console.log(sayHello());
+  try {
+    core.debug('Starting Action');
+
+    // Throw if event is not of type workflow_dispatch or pull_request?
+
+    // Get the most recent tag associated with the commit on the main branch
+
+    // Get the labels from the active pull request
+
+    // Resolve the version number based on the labels and the most recent tag
+
+    // Return the resolved version number as an output variable
+
+    console.log(sayHello());
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  } finally {
+    core.debug('Finished Action Run');
+  }
 };
 
 await main();
